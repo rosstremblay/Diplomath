ceb0b790a74f7eafa436e9e2a8f4ddd2891aa746
rosstremblay <rosstremblay@gmail.com>
2026-07-01 15:13:25 -0400

    Delete health.js

--- health.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-  body: JSON.stringify({ ok: true, scorecard: process.env.SCORECARD_KEY ? "set" : "not set" }),
-});
